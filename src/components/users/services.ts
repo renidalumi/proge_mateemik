221a81cd207d7349c5964e774e30bb1ae86925e4
renida <50323160+renidalumi@users.noreply.github.com>
2022-01-20 19:13:35 +0200

    Update services.ts

--- src/components/users/services.ts
+++ src/components/users/services.ts
@@ -1,58 +1,84 @@
-import pool from '../../database';
-import { getConnection } from 'typeorm';
-import db from "../../db";
+//import db from "../../db";
+import { FieldPacket, ResultSetHeader } from "mysql2";
+import pool from "../../database";
 import hashService from "../general/services/hashService";
-import {NewUsers, Users, UpdateUsers} from "./interfaces";
-import eUser from './entity';
-
-const connection = getConnection();
+import {INewUsers, IUsers, IUpdateUsers } from "./interfaces";
 
 const usersService = {
-    getAllUsers: async ()  => {
-    const users = await pool.query("SELECT * FROM users");
-    return users;
-    },
-    getUserById: (id: number): Users | undefined => {
-        const user = db.users.find((element) => element.id === id);
-        return user;
-    },
-    getUserByEmail: async (email: string) => {
-        const user = await connection.getRepository(eUser).findOne( {email});
-        return user;
-    },
-    createUser: async (newUsers: NewUsers) => {
-        try {
-            const hashedPassword = await hashService.hash(newUsers.password);
-            const user = {
-                ...newUsers,
-                password: hashedPassword,
-    
-            }
-            const result = await connection.getRepository(eUser).save(user);
-            return result.id;
-        } catch (error){
+    getAllUsers: async (): Promise<IUsers[] | false> => {
+        try { 
+            const [users] : [IUsers[], FieldPacket[]] =await pool.query(
+                "SELECT * FROM users"
+            );
+            return users;
+        } catch (error) {
             console.log(error);
             return false;
         }
-
     },
-    updateUser: (users: UpdateUsers): boolean => {
-        const {id, eesNimi, pereNimi} = users;
-        const index = db.users.findIndex((element) => element.id === id);
-        if (eesNimi){
-            db.users[index].eesNimi = eesNimi;
+    getUserById: async (id: number): Promise<IUsers | false> => {
+        try {
+            const [users] : [IUsers[], FieldPacket[]] =await pool.query(
+                'SELECT id, eesNimi, pereNimi, email FROM users WHERE id =? IS NULL LIMIT 1', [id]
+            );
+            return users[0];
+        } catch (error) {
+            console.log(error);
+            return false;
+            
         }
-        if (pereNimi){
-            db.users[index].pereNimi = pereNimi;
+        // const user = db.users.find((element) => element.id === id);
+        // return user;
+    },
+    getUserByEmail: async (email: string): Promise< IUsers | false> => {
+        try {
+            const [users]: [IUsers[], FieldPacket[]] = await pool.query(
+              "SELECT * FROM users WHERE email = ?",
+            [email]
+        );
+        return users[0];
+        } catch (error) {
+        console.log(error);
+        return false;
         }
-        return true;
-    },
-    removeUser:  (id: number): boolean => {
-        const index = db.users.findIndex((element) => element.id === id);
-        db.users.splice(index, 1);
-        return true;
-    },
+        },
     
-};
+    createUser: async (newUsers: INewUsers): Promise<number | false> => {
+        try {
+            const hashedPassword = await hashService.hash(newUsers.password);
+            const user = {
+            ...newUsers,
+            password: hashedPassword,
+        };
+        const [result]: [ResultSetHeader, FieldPacket[]] = await pool.query('INSERT INTO users SET ?', [user]);
+        return result.insertId;
+        } catch (error) {
+        console.log(error);
+        return false;
+        }
+    },
+
+    updateUser: async (user: IUpdateUsers): Promise<boolean> => {
+        try {
+            const userToUpdate = { ...user };
+            if (user.password) userToUpdate.password = await hashService.hash(user.password);
+            const result = await pool.query('UPDATE users SET ? WHERE id = ?', [userToUpdate, user.id]);
+            console.log(result);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+        },
+    removeUser: async (id: number): Promise<boolean> => {
+        try {
+            await pool.query('UPDATE users SET dateDeleted = ? WHERE id = ?', [new Date(), id]);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;   
+        }
+    },
+}; 
 
 export default usersService;
